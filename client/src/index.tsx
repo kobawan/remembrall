42da75101da86d640a4ec4d0756633b481e3e206
Sara Nordmyr da Cunha <saranordmyr@hotmail.com>
2019-01-20 18:32:11 +0100

    Organize client folder and add storage util

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,7 +4,7 @@ import { ApolloClient, HttpLink, InMemoryCache } from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 import { App } from "./components/App/App";
 
-import "./common.less";
+import "./styles/common.less";
 
 const client = new ApolloClient({
 	link: new HttpLink({ uri: "http://localhost:4000/graphql" }), // TODO Fix for prod
---
+++ client/src/utils/localStorage.ts
@@ -0,0 +1,27 @@
+export enum StorageKeys {
+	UserId = "UserId",
+}
+
+type StorageKeysType = string;
+
+export const storageKeysToDefaultMap: { [key in StorageKeys]: StorageKeysType } = {
+	[StorageKeys.UserId]: "",
+};
+
+export const getStorageKey = (key: StorageKeys): StorageKeysType => {
+    try {
+        const value = window.localStorage.getItem(key);
+        if (value === null) {
+            throw Error;
+        }
+        return JSON.parse(value);
+    } catch {
+        return storageKeysToDefaultMap[key];
+    }
+};
+
+export const setStorageKey = (key: StorageKeys, value: StorageKeysType) => {
+    try {
+        window.localStorage.setItem(key, JSON.stringify(value));
+    } catch {}
+};
