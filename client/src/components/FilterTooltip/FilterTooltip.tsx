d32ff974558a6c5c4e24b797afc45b7796cfb9f4
Sara Nordmyr da Cunha <saranordmyr@hotmail.com>
2019-08-15 12:36:37 +0200

    Rename checkbox folder

--- client/src/components/FilterTooltip/FilterTooltip.tsx
+++ client/src/components/FilterTooltip/FilterTooltip.tsx
@@ -2,7 +2,7 @@ import React, { useState, useContext } from "react";
 import "./filterTooltip.less";
 import { Overlay } from "../Overlay/Overlay";
 import { OverlayZIndex } from "../../types";
-import { Checkbox } from "../foo/Checkbox";
+import { Checkbox } from "../Checkbox/Checkbox";
 import { ReducerContext } from "../ColumnsManager/context";
 import { closeFilterTooltipAction, setFilterAction } from "../ColumnsManager/actions";
 
